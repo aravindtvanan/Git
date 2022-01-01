f4cbf8ee5b10d78b5a6269c58c8a25a7f2330302
aravindtvanan <aravindrocks26@gmail.com>
2021-12-31 22:00:46 -0800

    username fuctionality

--- src/test/java/com/test/Libgloab.java
+++ src/test/java/com/test/Libgloab.java
@@ -1,5 +1,11 @@
 package com.test;
 
 public class Libgloab {
+	public static  void username() {
+	System.out.println("username");
 
+	}
+public static void main(String[] args) {
+	username();
+}
 }
