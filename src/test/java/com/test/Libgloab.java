757c3b0e0edb81d8fe8962a932cf9de0e3aa9bc9
aravindtvanan <aravindrocks26@gmail.com>
2021-12-31 22:10:41 -0800

    password functionality

--- src/test/java/com/test/Libgloab.java
+++ src/test/java/com/test/Libgloab.java
@@ -1,5 +1,12 @@
 package com.test;
 
 public class Libgloab {
+	public static  void password() {
+		System.out.println("password");
 
-}
+		}
+	public static void main(String[] args) {
+		password();
+	}
+	}
+
